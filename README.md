c168a9007c26f221bba1ad0d74c392db1e6b3878
Pedro Suarez <psuar92@gmail.com>
2023-12-12 21:19:40 -0600

    Tiktok inspired app

---
+++ README.md
@@ -0,0 +1,8 @@
+# toktik
+
+A new Flutter project.
+
+An app I made inspired by tiktok, it was just for practice reasons. No login or menus applied yet.
+Might do it in the future to keep practicing
+
+I added the use of datasources and repositories.
---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,50 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Sint elit amet ut est.',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Eu voluptate qui id veniam laborum.',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Duis in nulla id incididunt consequat sunt.',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'Anim est eiusmod adipisicing in aliqua sit duis.',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'Enim enim deserunt elit ea duis non reprehenderit.',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'Est tempor exercitation est quis quis tempor eu.',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Nisi id id culpa cupidatat ullamco proident commodo.',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'Laboris cillum nisi exercitation ut est.',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+];
